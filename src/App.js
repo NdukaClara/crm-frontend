a32488c11cee811a99f5d09b260fc2d9bb09ae56
Nduka Clara <93293058+NdukaClara@users.noreply.github.com>
2022-05-16 01:43:30 +0100

    layout added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { EntryPage } from "./pages/entry/EntryPage";
 import { DefaultLayout } from "./layout/DefaultLayout";
+import { Dashboard } from "./pages/entry/dashboard/DashboardPage";
 function App() {
   return (
     <div>
       {/* <EntryPage /> */}
-      <DefaultLayout> //Dashboard </DefaultLayout>
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout>
     </div>
   );
 }
--- src/layout/partials/HeaderComponent.js
+++ src/layout/partials/HeaderComponent.js
@@ -6,20 +6,14 @@ export const Header = () => {
   return (
     <Navbar collapseOnSelect bg="info" variant="dark" expand="md">
       <Navbar.Brand>
-        <img src={logo} alt="logo" width="50px" className="nav-img" />
+        <img src={logo} alt="logo" width="50px" className="nav-img ms-4" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          <Nav.Link href="/dashboard" className="nav-list">
-            Dashboard
-          </Nav.Link>
-          <Nav.Link href="/tickets" className="nav-list">
-            Tickets
-          </Nav.Link>
-          <Nav.Link href="/logout" className="nav-list">
-            Logout
-          </Nav.Link>
+        <Nav className="ms-auto me-4">
+          <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+          <Nav.Link href="/tickets">Tickets</Nav.Link>
+          <Nav.Link href="/logout">Logout</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
